cbacd4e063cedb2b1ada121edbccc49147af8567
zefirka <wombtromb@gmail.com>
2015-08-16 12:48:36 +0300

    finished tokenizer

--- node/dvastula/index.js
+++ node/dvastula/index.js
@@ -9,14 +9,6 @@ function Dvastula(source){
 }
 
 function tokeinzer(source){
-  var seqs = source .replace(/\[/g, ' [ ')
-                    .replace(/\]/g, ' ] ')
-                    .replace(/\#.+(\n|$)/g, ''); //cut of comments
-
-}
-
-
-function tokenize(expr){
   function stringSwipeOn(string){
     return string
       .replace(/ /g, '__PROB__')
@@ -30,18 +22,13 @@ function tokenize(expr){
         .replace(/__NTAB__/g, '\t');
   }
 
-  var sequences = expr
-    .replace(/'\(.*?\)/g, function(e){
-      return '(quote ' + e.slice(1) + ' )';
-    })
-    .replace(/\~\(.*?\)/g, function(e){
-      return '(eval ' + e.slice(1) + ' )';
-    })
-    .replace(/\(/g, ' [ ')
-    .replace(/\)/g, ' ] ')
-    .replace(/\;.+(\n|$)/g, '')
-    .replace(/\'.*?\'/g, stringSwipeOn)
-    .replace(/\{.*?\}/g, stringSwipeOn);
 
-  return sequences.trim().split(/\s+/).map(stringSwipeOff);
+  var seqs = source .replace(/\[/g, ' [ ')
+                    .replace(/\]/g, ' ] ')
+                    .replace(/\#.+(\n|$)/g, '') //cut of comments
+                    .replace(/\'.*?\'/g, stringSwipeOn)
+                    .replace(/\{.*?\}/g, stringSwipeOn);
+
+  return seqs.trim().split(/\s+/).map(stringSwipeOff);
+
 }
