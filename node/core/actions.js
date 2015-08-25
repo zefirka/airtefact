14acd23453db0804dcdcd7017e297ddbb4198a73
Trdat Mkrtchyan <zeffirsky@yandex-team.ru>
2015-08-25 12:51:39 +0300

    Changed compiler

--- node/core/actions.js
+++ node/core/actions.js
@@ -5,21 +5,23 @@ var s2Compiler = require('../dvastula/compiler');
 var fs = require('fs');
 
 module.exports = [
+
   function (socket){
     socket.on('play', function(code){
       var js = s2Compiler(code);
       var timestamp = new Date().getTime().toString().slice(3),
           filename = config.files + '/myId.' + timestamp;
 
-      console.log('writing file ', filename);
       fs.writeFile(filename + '.js', js, function(err, data){
+
         if(err){
-          throw 'Fuck you';
+          console.log(err);
+          console.error('cant write file');
         }
 
         socket.emit('compile:ready', filename);
-
       });
     });
   }
+
 ];
