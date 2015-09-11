3576295ccdfd43a318da0f657444bdb9dbd97265
Trdat Mkrtchyan <zeffirsky@yandex-team.ru>
2015-09-11 21:34:39 +0300

    Added tests

--- common/test/utils.spec.js
+++ common/test/utils.spec.js
@@ -3,7 +3,8 @@ var utils = require('../utils');
 
 describe('Module: Common/utils'.bold, function (){
   var forIn = utils.forIn,
-      forMap = utils.forMap;
+      forMap = utils.forMap,
+      forReduce = utils.forReduce;
 
   describe(':: forIn', function (){
       var ins = { a: 1, b: 2, c: 3 };
@@ -42,15 +43,37 @@ describe('Module: Common/utils'.bold, function (){
   describe(':: forReduce', function (){
       var ins = { a: 1, b: 2, c: 3 };
 
-      vals = forMap(ins, function(val, key){
-        return val + key;
-      });
+      vals = forReduce(ins, function(res, val, key){
+        res.push(key + val);
+        return res;
+      }, []);
 
       it('should transform values', function(){
-        // TODO
-        expect(true).toEqual(true);
+        expect(vals).toEqual(['a1','b2','c3']);
       });
 
+  });
+
+  describe(':: camelCase', function () {
+    var inps = [
+      'allahu-akbar',
+      'allahu_akbar',
+      '-allahu-akbar-',
+      '_allahu_akbar_'
+    ]
+
+    it('should transform lisp-case', function(){
+      expect(utils.camelCase(inps[0])).toEqual('allahuAkbar');
+    })
+
+    it('should not transform underscore-case', function(){
+      expect(utils.camelCase(inps[1])).toEqual('allahu_akbar');
+      expect(utils.camelCase(inps[3])).toEqual('_allahu_akbar_');
+    });
+
+    it('should transform first case', function () {
+      expect(utils.camelCase(inps[2])).toEqual('AllahuAkbar-');
+    });
   });
 
 });
