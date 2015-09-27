fbb6893049a3631751041dfa61c1d6d6effeb7a6
Trdat Mkrtchyan <zeffirsky@yandex-team.ru>
2015-09-27 21:37:39 +0300

    Added cells

---
+++ common/cell.js
@@ -0,0 +1,118 @@
+function Break(){}
+
+function execPipeline(pipeline, end, value){
+  if(value instanceof(Break)){
+    return;
+  }
+
+  if(pipeline.length){
+    return pipeline[0].call(this, value, function(transform){
+      return execPipeline.call(this, pipeline.slice(1), end, transform);
+    }, function(){
+      return new Break();
+    });
+  }else{
+    return end(value);
+  }
+}
+
+function Cell(value){
+  this.__handlers = [];
+  this.__pipeline = [];
+  this.__prev = null;
+
+  if(typeof value !== 'undefined'){
+    this.__value  = value;
+    this.value = value;
+  }
+
+  this.changed = false;
+}
+
+Object.defineProperty(Cell.prototype, 'value', {
+  get : function(){
+    this.changed = false;
+    return this.__value;
+  },
+  set : function(value){
+    this.__prev = this.__value;
+    this.emit(value);
+  }
+});
+
+Cell.prototype.emit = function(value){
+  execPipeline(this.__pipeline, function(result){
+    this.__value = result;
+    this.changed = true;
+    this.__handlers.forEach(function(handler){
+      handler.call(this, this.__value);
+    }.bind(this));
+  }.bind(this), value);
+};
+
+Cell.prototype.on = function(callback){
+  this.__handlers.push(callback);
+};
+
+Cell.prototype.filter = function(fn){
+  var ncell = new Cell();
+  ncell.pipe(function(value, next, stop){
+    return fn(value) === true ? next(value) : stop();
+  });
+  ncell.value = this.value;
+  return ncell;
+};
+
+Cell.prototype.map = function(fn){
+  var ncell = new Cell();
+  ncell.pipe(function(value, next){
+    return next(fn(value));
+  });
+  ncell.value = this.value;
+  return ncell;
+};
+
+Cell.prototype.reduce = function(fn, init){
+  var ncell = new Cell();
+  var self = this;
+  ncell.pipe(function(value, next){
+    return next(fn(ncell.__prev || init, value));
+  });
+  ncell.value = this.value;
+  return ncell;
+};
+
+Cell.prototype.pipe = function(fn){
+  this.__pipeline.push(fn);
+};
+
+function cell (cellar, def) {
+  if(typeof cellar === 'function'){
+    def = new Cell(def);
+    cellar(function(value){
+      def.value = value;
+    });
+    return def;
+  }else{
+    return new Cell(cellar);
+  }
+}
+
+function formula(deps, func, init ){
+  var cell = new Cell(init);
+
+  deps.forEach(function(dep){
+    dep.on(function(){
+      cell.value = func.apply(null, deps.map(function(d){
+        return d.value;
+      }));
+    });
+  });
+
+  return cell;
+}
+
+module.exports = {
+  cell : cell,
+  formula : formula
+};
